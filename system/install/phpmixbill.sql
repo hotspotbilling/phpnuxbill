65666ac998ea321eb76c325dc4badb009aa51698
Ibnu Maksum <ibnumaksum@gmail.com>
2022-09-08 16:32:12 +0700

    latest structure

--- system/install/phpmixbill.sql
+++ system/install/phpmixbill.sql
@@ -1,19 +1,23 @@
+--
+
+-- Database: `phpmixbill`
+
+--
+
+-- --------------------------------------------------------
+
+--
+
+-- Struktur dari tabel `tbl_appconfig`
+
+--
+
 CREATE TABLE
     `tbl_appconfig` (
         `id` int(11) NOT NULL,
         `setting` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
         `value` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
-
---
-
--- Dumping data untuk tabel `tbl_appconfig`
-
---
-
-INSERT INTO
-    `tbl_appconfig` (`id`, `setting`, `value`)
-VALUES (1, 'CompanyName', 'PHPMixBill'), (2, 'currency_code', 'Rp.'), (3, 'language', 'indonesia'), (4, 'show-logo', '1'), (5, 'nstyle', 'blue'), (6, 'timezone', 'Asia/Jakarta'), (7, 'dec_point', ','), (8, 'thousands_sep', '.'), (9, 'rtl', '0'), (10, 'address', ''), (11, 'phone', ''), (12, 'date_format', 'd M Y'), (13, 'note', 'Thank you...');
 
 -- --------------------------------------------------------
 
@@ -49,7 +53,7 @@ CREATE TABLE
         `fullname` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
         `address` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
         `phonenumber` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '0',
-        `email` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '1',
+        `email` varchar(128) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '1',
         `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
         `last_login` datetime DEFAULT NULL
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
@@ -69,36 +73,6 @@ CREATE TABLE
         `folder` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
         `author` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
-
---
-
--- Dumping data untuk tabel `tbl_language`
-
---
-
-INSERT INTO
-    `tbl_language` (
-        `id`,
-        `name`,
-        `folder`,
-        `author`
-    )
-VALUES (
-        1,
-        'Indonesia',
-        'indonesia',
-        'Ismail Marzuqi'
-    ), (
-        2,
-        'English',
-        'english',
-        'Ismail Marzuqi'
-    ), (
-        4,
-        'Türkçe',
-        'turkish',
-        'Goktug Bogac Ogel'
-    );
 
 -- --------------------------------------------------------
 
@@ -148,19 +122,23 @@ CREATE TABLE
 CREATE TABLE
     `tbl_payment_gateway` (
         `id` int(11) NOT NULL,
-        `username` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-        `gateway` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'xendit | midtrans',
+        `username` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+        `gateway` varchar(32) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'xendit | midtrans',
         `gateway_trx_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
-        `plan_name` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-        `routers` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-        `price` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+        `plan_id` int(11) NOT NULL,
+        `plan_name` varchar(40) COLLATE utf8mb4_general_ci NOT NULL,
+        `routers_id` int(11) NOT NULL,
+        `routers` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+        `price` varchar(40) COLLATE utf8mb4_general_ci NOT NULL,
         `pg_url_payment` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
-        `pg_request` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+        `payment_method` varchar(32) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+        `payment_channel` varchar(32) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+        `pg_request` text COLLATE utf8mb4_general_ci,
         `pg_paid_response` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
         `expired_date` datetime DEFAULT NULL,
         `created_date` datetime NOT NULL,
         `paid_date` datetime DEFAULT NULL,
-        `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 unpaid 2 paid'
+        `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 unpaid 2 paid 3 failed 4 canceled'
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -192,7 +170,8 @@ CREATE TABLE
         `validity_unit` enum('Mins', 'Hrs', 'Days', 'Months') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
         `shared_users` int(10) DEFAULT NULL,
         `routers` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-        `pool` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
+        `pool` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+        `enabled` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0 disabled\r\n'
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -226,7 +205,8 @@ CREATE TABLE
         `ip_address` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
         `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
         `password` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-        `description` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
+        `description` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+        `enabled` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0 disabled'
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -271,34 +251,6 @@ CREATE TABLE
         `last_login` datetime DEFAULT NULL,
         `creationdate` datetime NOT NULL
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
-
---
-
--- Dumping data untuk tabel `tbl_users`
-
---
-
-INSERT INTO
-    `tbl_users` (
-        `id`,
-        `username`,
-        `fullname`,
-        `password`,
-        `user_type`,
-        `status`,
-        `last_login`,
-        `creationdate`
-    )
-VALUES (
-        1,
-        'admin',
-        'Administrator',
-        'd033e22ae348aeb5660fc2140aec35850c4da997',
-        'Admin',
-        'Active',
-        '2022-09-06 16:09:50',
-        '2014-06-23 01:43:07'
-    );
 
 -- --------------------------------------------------------
 
@@ -592,3 +544,71 @@ ALTER TABLE
     `tbl_voucher` MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 COMMIT;
+
+--
+
+-- Dumping data untuk tabel `tbl_appconfig`
+
+--
+
+INSERT INTO
+    `tbl_appconfig` (`id`, `setting`, `value`)
+VALUES (1, 'CompanyName', 'PHPMixBill'), (2, 'currency_code', 'Rp.'), (3, 'language', 'indonesia'), (4, 'show-logo', '1'), (5, 'nstyle', 'blue'), (6, 'timezone', 'Asia/Jakarta'), (7, 'dec_point', ','), (8, 'thousands_sep', '.'), (9, 'rtl', '0'), (10, 'address', ''), (11, 'phone', ''), (12, 'date_format', 'd M Y'), (13, 'note', 'Thank you...');
+
+--
+
+-- Dumping data untuk tabel `tbl_users`
+
+--
+
+INSERT INTO
+    `tbl_users` (
+        `id`,
+        `username`,
+        `fullname`,
+        `password`,
+        `user_type`,
+        `status`,
+        `last_login`,
+        `creationdate`
+    )
+VALUES (
+        1,
+        'admin',
+        'Administrator',
+        'd033e22ae348aeb5660fc2140aec35850c4da997',
+        'Admin',
+        'Active',
+        '2022-09-06 16:09:50',
+        '2014-06-23 01:43:07'
+    );
+
+--
+
+-- Dumping data untuk tabel `tbl_language`
+
+--
+
+INSERT INTO
+    `tbl_language` (
+        `id`,
+        `name`,
+        `folder`,
+        `author`
+    )
+VALUES (
+        1,
+        'Indonesia',
+        'indonesia',
+        'Ismail Marzuqi'
+    ), (
+        2,
+        'English',
+        'english',
+        'Ismail Marzuqi'
+    ), (
+        4,
+        'Türkçe',
+        'turkish',
+        'Goktug Bogac Ogel'
+    );
