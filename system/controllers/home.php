9ca2933b5d4c9c0e39be9e5e5ffadb183265c962
Ibnu Maksum <ibnumaksum@gmail.com>
2022-09-11 11:02:30 +0700

    get unpaid

--- system/controllers/home.php
+++ system/controllers/home.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
-* PHP Mikrotik Billing (https://ibnux.github.io/phpmixbill/)
-**/
+ * PHP Mikrotik Billing (https://ibnux.github.io/phpmixbill/)
+ **/
 _auth();
-$ui->assign('_title', $_L['Dashboard'].' - '. $config['CompanyName']);
+$ui->assign('_title', $_L['Dashboard'] . ' - ' . $config['CompanyName']);
 
 $user = User::_info();
 $ui->assign('_user', $user);
@@ -11,5 +12,11 @@ $ui->assign('_user', $user);
 //Client Page
 $bill = User::_billing();
 $ui->assign('_bill', $bill);
+
+
+$ui->assign('unpaid', ORM::for_table('tbl_payment_gateway')
+    ->where('username', $user['username'])
+    ->where('status', 1)
+    ->find_one());
 
 $ui->display('user-dashboard.tpl');
