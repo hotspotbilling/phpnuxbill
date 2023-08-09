755e857979001169a62698665a37d0d1dee0589a
Ibnu Maksum <ibnumaksum@gmail.com>
2023-08-09 10:49:29 +0700

    Add Money Format to Class

--- system/autoload/Lang.php
+++ system/autoload/Lang.php
@@ -4,12 +4,21 @@
  * PHP Mikrotik Billing (https://github.com/hotspotbilling/phpnuxbill/)
  **/
 
-class Lang {
-    public static function T($var) {
+class Lang
+{
+    public static function T($var)
+    {
         return Lang($var);
     }
 
-    public static function htmlspecialchars($var) {
+    public static function htmlspecialchars($var)
+    {
         return htmlspecialchars($var);
+    }
+
+    public static function moneyFormat($var)
+    {
+        global $config;
+        return $config['currency_code'] . ' ' .number_format($var, 0, $config['dec_point'], $config['thousands_sep']);
     }
 }
