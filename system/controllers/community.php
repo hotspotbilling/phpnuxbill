e5828169824b640d0e1887fb16beddde64e47c3d
Ibnu Maksum <ibnumaksum@gmail.com>
2024-05-21 10:58:14 +0700

    100 update list

--- system/controllers/community.php
+++ system/controllers/community.php
@@ -14,8 +14,8 @@ $ui->assign('_admin', $admin);
 switch ($action) {
     case 'rollback':
         $ui->assign('_title', 'Rollback Update');
-        $masters = json_decode(Http::getData("https://api.github.com/repos/hotspotbilling/phpnuxbill/commits",['User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:125.0) Gecko/20100101 Firefox/125.0']), true);
-        $devs = json_decode(Http::getData("https://api.github.com/repos/hotspotbilling/phpnuxbill/commits?sha=Development",['User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:125.0) Gecko/20100101 Firefox/125.0']), true);
+        $masters = json_decode(Http::getData("https://api.github.com/repos/hotspotbilling/phpnuxbill/commits?per_page=100",['User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:125.0) Gecko/20100101 Firefox/125.0']), true);
+        $devs = json_decode(Http::getData("https://api.github.com/repos/hotspotbilling/phpnuxbill/commits?sha=Development&per_page=100",['User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:125.0) Gecko/20100101 Firefox/125.0']), true);
 
         $ui->assign('masters', $masters);
         $ui->assign('devs', $devs);
