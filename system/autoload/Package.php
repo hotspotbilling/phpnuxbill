7d3afa091fa21f382fe0a341f4795d0e7e5b38f5
Focuslinkstech <45756999+Focuslinkstech@users.noreply.github.com>
2024-07-26 09:49:52 +0100

    Bugs Fix
    lots has been done that i cant even recalled, but i think all identify bugs is fix, i urge you all to give it test and report any issues arising.

--- system/autoload/Package.php
+++ system/autoload/Package.php
@@ -235,7 +235,7 @@ class Package
                 }
             }
 
-            if ($b['status'] == 'off') {
+            if ($b['status'] == 'on') {
                 $dvc = Package::getDevice($p);
                 if ($_app_stage != 'demo') {
                     if (file_exists($dvc)) {
